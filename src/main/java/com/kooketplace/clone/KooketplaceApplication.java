4557e7d767805b077b33c4e08c6f3349c77df07e
KoorunG <onlyzion10@naver.com>
2022-11-22 13:47:28 +0900

    init project

---
+++ src/main/java/com/kooketplace/clone/KooketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.kooketplace.clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KooketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KooketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kooketplace/clone/KooketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kooketplace.clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KooketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
